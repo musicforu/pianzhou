f414a2aa99b279d1c59a542fc2f2bf79374903d7
musicforu <799611732@qq.com>
2016-01-12 22:16:18 +0800

    fourth

--- config.py
+++ config.py
@@ -10,6 +10,18 @@ class Config:
 	SQLALCHEMY_COMMIT_ON_TEARDOWN=True
 	FLASKY_ADMIN='Superman'
 	DEBUG=True
+	FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
+	FLASKY_MAIL_SENDER='Flasky Admin <flasky@666.com>'
+	FLASKY_ADMIN='[Flasky]'
+	FLASKY_POSTS_PER_PAGE=20
+	FLASKY_FOLLOWERS_PER_PAGE = 50
+	FLASKY_COMMENTS_PER_PAGE =50
+	SQLALCHEMY_RECORD_QUERIES = True
+	FLASKY_SLOW_DB_QUERY_TIME=0.5
+	MAIL_SERVER='smtp.qq.com'
+	MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+	MAIL_PORT=25
 	SQLALCHEMY_DATABASE_URI='postgres://ehvfwslnxkxmxh:3s_8uTwvXaBdug-yinVFlDdRkl@ec2-54-83-52-144.compute-1.amazonaws.com:5432/dbvamn4raq0k91'
 
 	@staticmethod
