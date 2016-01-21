32c60625f586401c886ae42f00e5f29c85f7f5ed
musicforu <799611732@qq.com>
2016-01-22 00:54:30 +0800

    twentytwo

--- app/main/views.py
+++ app/main/views.py
@@ -195,7 +195,7 @@ def followers(username):
 		error_out=False)
 	follows=[{'user':item.follower,'timestamp':item.timestamp}
 			for item in pagination.items]
-	return render_template('followers.html',user=user,title='被以下用户关注',endpoint='.followers',pagination=pagination,follows=follows)
+	return render_template('followers.html',user=user,title=u'被以下用户关注',endpoint='.followers',pagination=pagination,follows=follows)
 
 @main.route('/followed-by/<username>')
 def followed_by(username):
@@ -209,7 +209,7 @@ def followed_by(username):
         error_out=False)
     follows = [{'user': item.followed, 'timestamp': item.timestamp}
                for item in pagination.items]
-    return render_template('followers.html', user=user, title="关注的人",
+    return render_template('followers.html', user=user, title=u"关注的人",
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
 
